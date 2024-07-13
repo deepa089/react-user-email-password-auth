d661ce26eeb775471a590a013ba19b828deebb65
deepa robisina <robisina.deepa@bracits.com>
2024-07-13 11:10:30 +0600

    4 done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,84 @@
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import app from "../../firebase.config";
+import { useState } from "react";
+import { FiEye, FiEyeOff } from "react-icons/fi";
+
 const Login = () => {
+    const [errorMsg, setErrorMsg] = useState("");
+    const [successMsg, setSuccessMsg] = useState("");
+    const [showPassword, setShowPassword] =useState(false);
+    const handleLogin = (e) => {
+        e.preventDefault();
+        setErrorMsg("");
+        setSuccessMsg("");
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        
+        const auth = getAuth(app);
+        if(password.length < 6){
+            setErrorMsg("Password must be 6 characters or more");
+            return;
+        } else if(!/[A-Z]/.test(password)){
+            setErrorMsg("Password must have at least one uppercase character");
+            return;
+        } 
+
+        signInWithEmailAndPassword(auth, email,password)
+        .then(result => {
+            console.log("result ::" + result);
+            setSuccessMsg("User Login Successfully")
+        })
+        .catch(error => {
+            setErrorMsg(error.message);
+            // console.log("error ::" + error.message);
+        })
+    }
+    // const handleShowPassword = () => {
+    //     setShowPassword(!showPassword)
+    // }
     return (
-        <div>
-            <h2>Login with email nad password</h2>
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <p className="py-6">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    </p>
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <form className="card-body" onSubmit={handleLogin}>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <div className="form-control relative">
+                                <input type={showPassword ? 'text' : 'password'}  name="password" placeholder="password" className="input input-bordered" required />
+                                <span className="absolute top-4 right-3" onClick={() => setShowPassword(!showPassword)}> {showPassword ? <FiEye></FiEye> : <FiEyeOff></FiEyeOff> }  </span>
+                            </div>
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                            
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                    { errorMsg &&
+                        <p className="text-red-700 font-semibold"> {errorMsg}</p>
+                    }
+                { successMsg &&
+                        <p className="text-green-700 font-semibold"> {successMsg}</p>
+                    }
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -6,31 +6,34 @@ import { FiEye, FiEyeOff } from "react-icons/fi";
 const Register = () => {
     const [errorMsg, setErrorMsg] = useState("");
     const [successMsg, setSuccessMsg] = useState("");
-    const [showPassword, setShowPassword] =useState(false);
+    const [showPassword, setShowPassword] = useState(false);
     const handleRegister = (e) => {
         e.preventDefault();
         setErrorMsg("");
         setSuccessMsg("");
         const email = e.target.email.value;
         const password = e.target.password.value;
+        const termAndCondition = e.target.termAndCondition.checked;
         const auth = getAuth(app);
-        if(password.length < 6){
+        if (password.length < 6) {
             setErrorMsg("Password must be 6 characters or more");
             return;
-        } else if(!/[A-Z]/.test(password)){
+        } else if (!/[A-Z]/.test(password)) {
             setErrorMsg("Password must have at least one uppercase character");
             return;
+        } else if (!termAndCondition) {
+            setErrorMsg("Ckeck terem & condition for Register")
         }
 
-        createUserWithEmailAndPassword(auth, email,password)
-        .then(result => {
-            console.log("result ::" + result);
-            setSuccessMsg("User Create Successfully")
-        })
-        .catch(error => {
-            setErrorMsg(error.message);
-            // console.log("error ::" + error.message);
-        })
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                console.log("result ::" + result);
+                setSuccessMsg("User Create Successfully")
+            })
+            .catch(error => {
+                setErrorMsg(error.message);
+                // console.log("error ::" + error.message);
+            })
     }
 
     const handleShowPassword = () => {
@@ -43,21 +46,25 @@ const Register = () => {
             <div className="">
                 <form onSubmit={handleRegister}>
                     <input className="mb-4 border-gray-300 p-2 w-1/4 bg-slate-200 rounded"
-                     required type="email" name="email" id="" /> <br />
-                    <div className="flex justify-center">
-                    <input className="mb-4 border-gray-300 p-2 w-1/4 bg-slate-200 rounded" 
-                    required type={showPassword ? 'text' : 'password'} name="password" id="" />
-                    <span className="float-le" onClick={() => setShowPassword(!showPassword)}> {showPassword ? <FiEye></FiEye> : <FiEyeOff></FiEyeOff> }  </span>
-                     </div> 
-                     <br />
+                        required type="email" name="email" id="" /> <br />
+                    <div className="relative">
+                        <input className="mb-4 border-gray-300 p-2 w-1/4 bg-slate-200 rounded"
+                            required type={showPassword ? 'text' : 'password'} name="password" id="" />
+                        <span className="absolute top-3" onClick={() => setShowPassword(!showPassword)}> {showPassword ? <FiEye></FiEye> : <FiEyeOff></FiEyeOff>}  </span>
+                    </div>
+                    <br />
+                    <div className="text-center w-3/4">
+                        <input className=" left-1" type="checkbox" name="termAndCondition" /> <br />
+                    </div>
+
                     <input className="mb-4 px-10 py-2 w-1/4 rounded btn btn-secondary" type="submit" value="Register" /> <br />
                 </form>
-                { errorMsg &&
-                        <p className="text-red-700 font-semibold"> {errorMsg}</p>
-                    }
-                { successMsg &&
-                        <p className="text-green-700 font-semibold"> {successMsg}</p>
-                    }
+                {errorMsg &&
+                    <p className="text-red-700 font-semibold"> {errorMsg}</p>
+                }
+                {successMsg &&
+                    <p className="text-green-700 font-semibold"> {successMsg}</p>
+                }
             </div>
         </div>
     );
